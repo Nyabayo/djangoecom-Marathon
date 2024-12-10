78819baed07566b92a229dc8a482e18c7df41a50
Nyabayo <ernestosindo9@gmail.com>
2024-12-11 00:11:59 +0300

    models

--- ecom1/ecom1app/admin.py
+++ ecom1/ecom1app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from ecom1app.models import Product, Category, Order, Customer
+
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(Customer)
+
--- ecom1/ecom1app/models.py
+++ ecom1/ecom1app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+class Product(models.Model):
+    name = models.CharField(max_length=1000)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    description = models.TextField(max_length=250, default='', blank=True, null=True)
+    image = models.ImageField(upload_to='uploads/products/')
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, default=1)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, default=1)
+    quantity = models.IntegerField(default=1)
+    address = models.CharField(max_length=100)
+    location = models.CharField(max_length=100, default='', blank=True)
+    phone = models.CharField(max_length=100)
+    date = models.DateField(default=datetime.date.today)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.product.name} {self.quantity}'
+
